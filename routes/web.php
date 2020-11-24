4039df3615a58008553edbfc6d7bec6bdf127407
Tornike Berekashvili <tornike.berekashvili@pm.me>
2020-11-24 21:16:28 +0400

    new routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ Auth::routes();
 
 Route::get('/', 'PostController@index')->name('index');
 
-Route::get('/profile/my_posts', 'PostController@index')->name('post.index');
+Route::get('/profile/{user}/my_posts', 'PostController@profile')->name('post.index');
 Route::get('/post/create', 'PostController@create')->name('post.create');
 Route::post('/post', 'PostController@store')->name('post.store');
 Route::get('/post/{post}', 'PostController@show')->name('post.show');
+Route::get('/post/{post}/edit', 'PostController@edit')->name('post.edit');
+Route::get('/post/{post}/delete', "PostController@destroy")->name('post.delete');
+Route::patch('/post/{post}', 'PostController@update')->name('post.update');
+
+Route::post('/post/filter', "PostController@filter")->name('post.filter');
