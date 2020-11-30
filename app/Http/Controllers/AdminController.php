5a47364ad40e195127f5e5fd8d191922844ea347
Tornike Berekashvili <tornike.berekashvili@pm.me>
2020-11-30 21:11:41 +0400

    updated controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\Category;
 use App\User;
+use App\Manufacturers;
+// use App\Carmodels;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-
+use App\Models;
 
 class AdminController extends Controller
 {
+
+    // for users
 
     public function index()
     {
@@ -28,77 +32,6 @@ class AdminController extends Controller
 
         return view('admin.admin_users', compact('users'));
     }
-
-    public function posts()
-    {
-        $posts = Post::paginate(12);
-
-        return view('admin.admin_posts', compact('posts'));
-    }
-
-    public function categories()
-    {
-        $parent_categories = Category::where('parent_id', '=', 0)->paginate(12);
-        $sub_categories = Category::where('parent_id', '>', 0)->paginate(12);
-
-        return view('admin.admin_categories', compact('parent_categories', 'sub_categories'));
-    }
-
-    public function edit_category(\App\Category $category)
-    {
-        $parent_categories = Category::where('parent_id', '=', 0)->get();
-        return view('admin.admin_categories_edit', compact('category', 'parent_categories'));
-    }
-
-    public function update_category(\App\Category $category)
-    {
-        $data = request()->validate([
-            'id' => 'required',
-            'parent_id' => 'required|integer',
-            'title' => 'required',
-            'category_id' => 'required|integer',
-        ]);
-
-        $category->update($data);
-
-        return redirect('/admin/categories')->with('success', 'Category updated successfully');
-    }
-
-
-    public function create_category()
-    {
-        $parent_categories = Category::where('parent_id', '=', 0)->get();
-
-        return view('admin.admin_categories_create', compact('parent_categories'));
-    }
-
-
-    public function store_category()
-    {
-        $data = request()->validate([
-            'parent_id' => 'required|integer',
-            'title' => 'required',
-            'category_id' => 'required|integer|unique:categories',
-        ]);
-
-        // auth()->user()->posts()->create([])
-
-        Category::create([
-            'parent_id' => $data['parent_id'],
-            'title' => $data['title'],
-            'category_id' => $data['category_id'],
-        ]);
-
-        return redirect('/admin/categories')->with('success', 'Category was created successfully');
-    }
-
-    public function delete_category(\App\Category $category)
-    {
-        Category::where("id", $category->id)->delete();
-        return redirect()->route('admin.categories')->with('danger', 'Category was removed from database');
-    }
-
-
 
     public function edit(User $user)
     {
@@ -129,11 +62,6 @@ class AdminController extends Controller
         return redirect()->route('admin.users')->with('danger', 'User was removed from database');
     }
 
-    public function delete_post(\App\User $post)
-    {
-        Post::where("id", $post->id)->delete();
-        return redirect()->route('admin.posts')->with('danger', 'Post was removed from database');
-    }
 
     public function create()
     {
@@ -151,8 +79,6 @@ class AdminController extends Controller
             'updated_at' => '',
         ]);
 
-        // auth()->user()->posts()->create([])
-
         User::create([
             'email' => $data['email'],
             'email_verified_at' => $data['email_verified_at'],
@@ -165,4 +91,147 @@ class AdminController extends Controller
 
         return redirect('/admin/users')->with('success', 'User was created successfully');
     }
+
+    // end user
+
+    // posts 
+
+    public function posts()
+    {
+        $posts = Post::paginate(12);
+
+        return view('admin.admin_posts', compact('posts'));
+    }
+
+    public function delete_post(\App\User $post)
+    {
+        Post::where("id", $post->id)->delete();
+        return redirect()->route('admin.posts')->with('danger', 'Post was removed from database');
+    }
+
+
+    // end posts
+
+
+
+    // categories
+
+
+    public function categories()
+    {
+        $parent_categories = Category::where('parent_id', '=', 0)->paginate(12);
+        $sub_categories = Category::where('parent_id', '>', 0)->paginate(12);
+
+        return view('admin.admin_categories', compact('parent_categories', 'sub_categories'));
+    }
+
+    public function edit_category(\App\Category $category)
+    {
+        $parent_categories = Category::where('parent_id', '=', 0)->get();
+        return view('admin.admin_categories_edit', compact('category', 'parent_categories'));
+    }
+
+    public function update_category(\App\Category $category)
+    {
+        $data = request()->validate([
+            'id' => 'required',
+            'parent_id' => 'integer',
+            'title' => 'required',
+        ]);
+
+        $category->update($data);
+
+        return redirect('/admin/categories')->with('success', 'Category updated successfully');
+    }
+
+
+    public function create_category()
+    {
+        $parent_categories = Category::where('parent_id', '=', 0)->get();
+
+        return view('admin.admin_categories_create', compact('parent_categories'));
+    }
+
+
+    public function store_category()
+    {
+        $data = request()->validate([
+            'parent_id' => 'required|integer',
+            'title' => 'required',
+        ]);
+
+        Category::create([
+            'parent_id' => $data['parent_id'],
+            'title' => $data['title'],
+        ]);
+
+        return redirect('/admin/categories')->with('success', 'Category was created successfully');
+    }
+
+    public function delete_category(\App\Category $category)
+    {
+        Category::where("id", $category->id)->delete();
+        return redirect()->route('admin.categories')->with('danger', 'Category was removed from database');
+    }
+
+    // end categories
+
+    // manufacturers
+    public function manufacturers()
+    {
+        $manufacturers = Manufacturers::paginate(12);
+        return view('admin.admin_manufacturers', compact('manufacturers'));
+    }
+
+    public function create_manufacturer()
+    {
+        return view('admin.admin_manufacturers_create');
+    }
+
+
+    public function store_manufacturer()
+    {
+        $data = request()->validate([
+            'name' => 'required|unique:manufacturers',
+        ]);
+
+        Manufacturers::create([
+            'name' => $data['name'],
+        ]);
+
+        return redirect('/admin/manufacturers')->with('success', 'Manufacturer was created successfully');
+    }
+
+
+
+    public function edit_manufacturer(\App\Manufacturers $manufacturer)
+    {
+        return view('admin.admin_manufacturers_edit', compact('manufacturer'));
+    }
+
+    public function update_manufacturer(\App\Manufacturers $manufacturer)
+    {
+        $data = request()->validate([
+            'name' => 'string',
+        ]);
+
+        $manufacturer->update($data);
+
+        return redirect('/admin/manufacturers')->with('success', 'Manufacturer updated successfully');
+    }
+
+    public function delete_manufacturer(\App\Manufacturers $manufacturer)
+    {
+        Manufacturers::where("id", $manufacturer->id)->delete();
+        return redirect()->route('admin.manufacturers')->with('danger', 'Manufacturer was removed from database');
+    }
+
+
+    // models
+
+    // public function models()
+    // {
+    //     $models = Carmodels::paginate(12);
+    //     return view('admin.admin_models', compact('models'));
+    // }
 }
