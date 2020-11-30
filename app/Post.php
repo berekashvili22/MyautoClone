b473e0933846c04cd2c56987325c46e9fe2c624f
Tornike Berekashvili <tornike.berekashvili@pm.me>
2020-11-30 21:10:59 +0400

    create post/manufact relationship

--- app/Post.php
+++ app/Post.php
@@ -12,9 +12,14 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
-}   
+
+    public function manufacturer()
+    {
+        return $this->belongsTo(Manufacturers::class, 'manufacturer_id');
+    }
+}
